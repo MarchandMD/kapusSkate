98aed18e6712b36b14ca2faaf02dfb00c0233ae5
Michael Marchand <michaeldavidmarchand@gmail.com>
2022-03-21 22:11:59 -0600

    update skates index

--- app/models/skate.rb
+++ app/models/skate.rb
@@ -1,3 +1,4 @@
 class Skate < ApplicationRecord
+  has_many :skaters
   validates :date, presence: true
 end
--- app/models/skater.rb
+++ app/models/skater.rb
@@ -1,3 +1,5 @@
 class Skater < ApplicationRecord
+  belongs_to :skate
+
   validates :name, presence: true
 end
