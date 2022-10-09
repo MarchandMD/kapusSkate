c9ca89e4767f6e0d06404623867bd661b9ab0d13
Michael Marchand <michaeldavidmarchand@gmail.com>
2022-10-09 17:09:33 -0600

    Update models, tables and seeds

--- spec/models/rink_spec.rb
+++ spec/models/rink_spec.rb
@@ -1,5 +1,45 @@
 require 'rails_helper'
 
 RSpec.describe Rink, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'Association' do
+    it { should have_many :skates }
+  end
+
+  subject do
+    described_class.new(name: 'Apex',
+                        address: '123 Main St',
+                        city: 'Westminster',
+                        state: 'CO',
+                        phone: '555-555-5555')
+  end
+
+  it 'is valid with valid attributes' do
+    expect(subject).to be_valid
+  end
+
+  it 'is not valid without a name' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'is not valid without a address' do
+    subject.address = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'is not valid without a city' do
+    subject.city = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'is not valid without a state' do
+    subject.state = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'is not valid without a phone' do
+    subject.phone = nil
+    expect(subject).to_not be_valid
+  end
 end
