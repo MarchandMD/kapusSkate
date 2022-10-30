efc718d9631599a8dfbb99ccd51934c639feb98b
Michael Marchand <michaeldavidmarchand@gmail.com>
2022-10-30 07:03:11 -0600

    Relates models to each other

--- app/models/skate.rb
+++ app/models/skate.rb
@@ -1,3 +1,11 @@
 class Skate < ApplicationRecord
   belongs_to :rink
+  has_many :skater_skates
+  has_many :skaters, through: :skater_skates
+
+  def self.next_scheduled_skate
+    today = Time.now.to_s.split[0]
+    next_skate = where('date >= ?', today).order(:date).first
+    next_skate.date.strftime("%A %B %d, %Y") unless next_skate.nil?
+  end
 end
--- app/models/skater.rb
+++ app/models/skater.rb
@@ -1,2 +1,4 @@
 class Skater < ApplicationRecord
+  has_many :skater_skates
+  has_many :skates, through: :skater_skates
 end
