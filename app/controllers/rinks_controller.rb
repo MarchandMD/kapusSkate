8e7cf3bc21a30e949613c290c3b541922d9f6cf8
Michael Marchand <michaeldavidmarchand@gmail.com>
2022-10-30 07:04:24 -0600

    Updates controllers and views

--- app/controllers/rinks_controller.rb
+++ app/controllers/rinks_controller.rb
@@ -4,4 +4,19 @@ class RinksController < ApplicationController
     @rinks = Rink.all
   end
 
+  def new; end
+
+  def create
+    Rink.create!(rink_params)
+
+    redirect_to '/rinks'
+  end
+
+  private
+
+  def rink_params
+    params.permit(:name, :address, :city, :state, :phone)
+  end
+
+
 end
