f924d96e2ba3a501c3afc0c86b011300895df26e
Michael Marchand <michaeldavidmarchand@gmail.com>
2022-10-30 07:02:44 -0600

    Updates the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'welcome#index'
   get '/rinks', to: 'rinks#index'
+  get '/rinks/new', to: 'rinks#new'
+  post '/rinks', to: 'rinks#create'
   resources :skates
   resources :skaters
   get '/about/index', to: 'about#index', as: 'about'
