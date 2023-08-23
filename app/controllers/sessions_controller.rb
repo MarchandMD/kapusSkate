d63d78931b13a10115ef2591b1bf4677bba19909
Michael Marchand <35391349+MarchandMD@users.noreply.github.com>
2023-08-22 18:46:32 -0600

    adds session controller action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,10 +2,10 @@ class SessionsController < ApplicationController
   def omniauth
     user = User.from_omniauth(request.env['omniauth.auth'])
     if user.valid?
-        session[:user_id] = user.id
-        redirect_to user_path(user)
+      session[:user_id] = user.id
+      redirect_to user_path(user)
     else
-      redirect_to '/login' 
+      redirect_to '/login'
     end
   end
 end
